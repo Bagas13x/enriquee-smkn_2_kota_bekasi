afb301a3190c6133333a4e1c86c81474dd71bdbf
Afiss <afisfisabilillah21@gmail.com>
2026-02-06 15:56:50 +0700

    fixed dummy data

--- components/Services.tsx
+++ components/Services.tsx
@@ -2,20 +2,37 @@ import { Service } from "@/types/services"
 import { title } from "process"
 import ServiceCard from "./ui/service-card"
 import ServiceImage from '@/public/assets/images/service-image.webp'
+import BukuCerita from '@/public/assets/images/buku_cerita_anak .webp'
+import Mockup from '@/public/assets/images/mockup.webp'
+import Poster from '@/public/assets/images/poster.webp'
+import Hand from '@/public/assets/images/hand-writing.webp'
+
 export default function Services() {
     const servicesm: Service[] = [
         {
-            title: "web development",
-            description: "lorem ipsum dolor sit amet",
-            image: ServiceImage,
-            category: ["web", "development"]
+            title: "Desain Buku Cerita Anak",
+            description: "Kami menyediakan layanan desain buku cerita anak yang menarik dan edukatif. Tim kami akan bekerja sama dengan Anda untuk menciptakan ilustrasi yang memukau dan tata letak yang sesuai dengan usia target pembaca.",
+            image: BukuCerita,
+            category: ["desain", "Illustrasi"]
         },
         {
-            title: "web development",
-            description: "lorem ipsum dolor sit amet",
-            image: ServiceImage,
-            category: ["web", "development"]
+            title: "Mockup Desain",
+            description: "Kami menawarkan layanan pembuatan mockup desain yang realistis untuk membantu Anda memvisualisasikan produk akhir sebelum produksi. Mockup kami dirancang dengan detail tinggi untuk memberikan gambaran yang jelas tentang bagaimana desain Anda akan terlihat di dunia nyata.",
+            image: Mockup,
+            category: ["Desain", "website"]
+        },{
+            title: "Desain Poster",
+            description: "Kami menyediakan layanan desain poster yang kreatif dan menarik untuk berbagai keperluan, seperti promosi acara, iklan produk, atau kampanye sosial. Tim desainer kami akan bekerja sama dengan Anda untuk menciptakan poster yang efektif dalam menyampaikan pesan Anda kepada audiens target.",
+            image: Poster,
+            category: ["Desain", "Ilustrasi"]
+        },
+        {
+            title: "Personal Commission Art",
+            description: "Personalisasi seni komisi adalah layanan di mana Anda dapat memesan karya seni yang dibuat khusus sesuai dengan preferensi dan kebutuhan Anda. Tim seniman kami akan bekerja sama dengan Anda untuk menciptakan karya seni yang unik dan bermakna, mulai dari potret pribadi hingga ilustrasi khusus untuk proyek tertentu.",
+            image: Hand,
+            category: ["Desain", "Ilustrasi"]   
         }
+
     ]
     return (
         <>
