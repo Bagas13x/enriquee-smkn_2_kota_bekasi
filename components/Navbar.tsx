636e59a70c3775a0c5adefd79bf4021385d0c595
alf4ridzi <aalalfaridzi9@gmail.com>
2026-02-06 09:27:10 +0700

    add logo smkn2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import Link from "next/link";
+
+const MenuItems = [
+
+]
 
 export default function Navbar() {
     return (
-        <>
-        </>
+        <nav>
+            <div>
+                <div>
+                    <Link href="/">
+                        <div>
+                            
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </nav>
     )
 }
