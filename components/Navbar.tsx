cdbde63ea1f5801bf045c155c62ca8479c7705ab
alf4ridzi <aalalfaridzi9@gmail.com>
2026-02-06 09:33:17 +0700

    terbaru

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Smkn2 from "@/public/assets/images/smkn2.webp";
 
 const MenuItems = [
-
+    { name: "navbar test 1", href: "#about"}
 ]
 
 export default function Navbar() {
@@ -12,9 +14,36 @@ export default function Navbar() {
                 <div>
                     <Link href="/">
                         <div>
-                            
+                            <Image 
+                                src={Smkn2}
+                                width={40}
+                                height={40}
+                                alt="Logo SMKN 2 KOTA BEKASI"
+                            />
+
                         </div>
                     </Link>
+                    <div className="hidden lg:flex items-center gap-8">
+                        <Link 
+                        href={"#about"}
+                        className="relative font-bold"
+                        >
+
+                        Test
+                        </Link>
+                        <Link href={"#about"}>
+
+                        Test
+                        </Link>
+                        <Link href={"#about"}>
+
+                        Test
+                        </Link>
+                        <Link href={"#about"}>
+
+                        Test
+                        </Link>
+                    </div>
                 </div>
             </div>
         </nav>
